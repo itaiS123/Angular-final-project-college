b9960340d18e1e20d914f88be3eaa48dfac180f7
Itai <itaishur@gmail.com>
2025-11-25 14:48:45 +0200

    add user component (son), hove some errores connected to servcie. do i have to have a constructor ?

--- src/user/user.ts
+++ src/user/user.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserI } from '../app/models/user-i';
 
 @Component({
   selector: 'app-user',
@@ -6,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
-export class User {
+export class User implements UserI{
+  id!: number;
+  name!: string
+  password!: string;
 
+  constructor(id!: number, name!: string, password!: string) {
+    this.id = id;
+    this.name = name;
+    this.password = password;
+  }
 }
